773f425a6263738b72159ed286283c9260cc9c6f
veeteefunk <victormarinviloria@gmail.com>
2019-07-16 17:43:49 +0200

    Updates

---
+++ precache-manifest.9f7a8f8df97e28bed11253401b792ab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1f5fe23c8c00c1b173ae51ba206631",
+    "url": "/MyFavoriteWords/index.html"
+  },
+  {
+    "revision": "bb0d43cdcfc283fbad53",
+    "url": "/MyFavoriteWords/static/css/main.50b17420.chunk.css"
+  },
+  {
+    "revision": "b9522c995c081ae55605",
+    "url": "/MyFavoriteWords/static/js/2.b6856631.chunk.js"
+  },
+  {
+    "revision": "bb0d43cdcfc283fbad53",
+    "url": "/MyFavoriteWords/static/js/main.0beb4c6c.chunk.js"
+  },
+  {
+    "revision": "091f460522be94d68bf0",
+    "url": "/MyFavoriteWords/static/js/runtime~main.0b83e010.js"
+  }
+]);
